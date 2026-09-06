115368cfd01569f24ea84bc5e8161469db4bac91
itachibhai453-stack <itachibhai453@gmail.com>
2026-09-06 22:51:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,13 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "your_api_hash_here")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token_here")
     
+    # MongoDB Database Details
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://username:password@cluster.mongodb.net/myDatabase")
+    DB_NAME = os.environ.get("DB_NAME", "RenamerBotDB")
+    
     # Optional Admin ID
     ADMIN = [int(admin) if admin.isdigit() else admin for admin in os.environ.get('ADMIN', '').split()]
     
     # Download Location
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-  
+    
